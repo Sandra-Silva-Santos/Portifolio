b50423c8950868804381d08fdaa236beebaedbaa
Sandra <99267898+Sandra-Silva-Santos@users.noreply.github.com>
2022-09-26 00:18:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,11 @@ const body = document.querySelector('body');
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
-        this.textContent= '  Light Mode';
         body.style.background = 'white';
-        body.style.color = '#212529';
+        body.style.color = 'black';
         body.style.transition = '2s';
     }else{
-        this.textContent = '  Dark Mode';
-        body.style.background = '#212529';
+        body.style.background = 'black';
         body.style.color = 'white';
         body.style.transition = '2s';
     }
